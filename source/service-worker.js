fd3ba79f50d6fce9cfbd63e6a703644efc8cbac3
lvonatzigen <lukasvonatzigen@gmail.com>
2026-02-04 20:27:10 +0100

    nice app-symbols

--- source/service-worker.js
+++ source/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "time-converter-v1";
+const CACHE_NAME = "time-converter-v2";
 const FILES_TO_CACHE = [
   "./",
   "./index.html",
@@ -9,6 +9,7 @@ const FILES_TO_CACHE = [
   "./icon-512.png"
 ];
 
+// Install SW and cache all files
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
@@ -16,21 +17,21 @@ self.addEventListener("install", event => {
   self.skipWaiting();
 });
 
+// Activate and remove old caches
 self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys().then(keys =>
       Promise.all(
-        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
       )
     )
   );
   self.clients.claim();
 });
 
+// Serve cached content first
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(
-      response => response || fetch(event.request)
-    )
+    caches.match(event.request).then(resp => resp || fetch(event.request))
   );
 });
